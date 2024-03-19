11316e02ee15aa3ae6a0a016aa2bd287ddede4b7
RiccardoAntonini95 <riccardo.antonini95@gmail.com>
2024-03-19 20:17:22 +0100

    fix string telefono + link nav

--- Models/Users.cs
+++ Models/Users.cs
@@ -31,6 +31,7 @@ namespace ClinicaCaniZzoo.Models
         public string CodiceFiscale { get; set; }
 
         [StringLength(15)]
+        [RegularExpression(@"^\d+$", ErrorMessage = "Il campo telefono deve contenere solo numeri interi.")]
         public string Telefono { get; set; }
 
         [StringLength(100)]
