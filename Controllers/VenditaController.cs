0fbd9377c3fe0874db32dd9335f8417bc89b5104
Ihor Hovdan <ihor99@outlook.it>
2024-03-20 16:20:40 +0100

    lol

---
+++ Controllers/VenditaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClinicaCaniZzoo.Controllers
+{
+    public class VenditaController : Controller
+    {
+        // GET: Vendita
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
